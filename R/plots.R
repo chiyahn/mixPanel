786a201a35a048642fb192d6007049e564151ebc
Chiyoung Ahn <chiyahn@hotmail.com>
2016-12-08 01:52:01 -0800

    plot function added.

---
+++ R/plots.R
@@ -0,0 +1,38 @@
+#' Draws a plot for an MDP model.
+#' @export
+#' @title PlotMDPModel
+#' @name PlotMDPModel
+#' @param mdp.model MDP.model instance
+#' @param separate Determines whether a separate plot for each
+#' component is going to be produced
+#' @return corresponding ggplot2 object
+PlotMDPModel <- function (mdp.model, separate = FALSE) {
+  y <- mdp.model$y
+  s <- 0
+  if (!is.null(mdp.model$theta$rho))
+    s <- nrow(mdp.model$theta$rho)
+  N <- ncol(y)
+  T <- nrow(y) - s
+  
+  y.transposed <- data.frame(t(y))
+  colnames(y.transposed) <- (-s+1):T
+  y.df <- reshape::melt(cbind(id = as.factor(1:N), 
+                              component = as.factor(mdp.model$components),
+                              y.transposed), 
+                        id = c('id', 'component'))
+  colnames(y.df)[colnames(y.df)=='value'] <- 'y'
+  colnames(y.df)[colnames(y.df)=='variable'] <- 't'
+  
+  
+  if (!separate)
+    return (ggplot(y.df, aes(x = t, y = y, group = id, color = component)) +
+      geom_point(size = 2) +
+      geom_line(size = 1.2, alpha = 0.5))
+  
+  
+  ggplot(y.df, aes(x = t, y = y, group = id)) +
+    geom_point(size = 2) +
+    geom_line(size = 1.2, alpha = 0.5, aes(shape = id)) +
+    facet_wrap(~component,
+               nrow = 1)
+}
